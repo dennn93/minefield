8de00a8a6690942544fe8063bd3b7cedb2d7f0d1
dennn93 <dennn93@mail.ru>
2020-01-17 23:10:01 +0300

    add proj

---
+++ GamePrincess/Game.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrincess
+{
+   
+    class Game
+    {
+        GameField gf;
+        Player player;
+        public Game (GameField Agf, Player Aplayer)
+        {
+            gf = Agf;
+            player = Aplayer;
+        }
+        public void Start(bool showTraps)
+        {
+            bool gameOver = false;
+            Console.Clear();
+            gf.Print(showTraps);
+            PutStringToConsole("Try to Finish!", 0, GameField.size + 3);
+            PutStringToConsole("Use arrows keys", 0, GameField.size + 4);
+            Console.SetCursorPosition(0,0);
+            
+            while (!gameOver)
+            {
+                Position playerPrevPos = (Position)player.pos.Clone();
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                //isMoved равно true, если игрок продвинулся => есть что отрисовать
+                bool isMoved = MovePlayer(keyInfo);
+                if (isMoved)
+                {
+                    //Поставить знак пустой клетке на месте, где был игрок
+                    PutCharToConsole('.', playerPrevPos.X , playerPrevPos.Y);
+                    //Игрок добрался до принцессы
+                    if (player.pos == gf.finalPos)
+                    {
+                        //W - win
+                        PutCharToConsole('W', player.pos.X , player.pos.Y);
+                        ClearLineFrom(0, GameField.size + 5);
+                        PutStringToConsole("You are winner!", 0, GameField.size + 5);
+                        gameOver = true;
+                        break;
+                    }
+                    //Игрок налетел на ловушку
+                    else if (gf.field[player.pos.X, player.pos.Y] != 0)
+                    {
+                        player.hp -= gf.field[player.pos.X, player.pos.Y];
+                        if (player.hp < 1)
+                        {
+                            PutCharToConsole('X', player.pos.X , player.pos.Y);
+                            ClearLineFrom(0, GameField.size + 5);
+                            PutStringToConsole("You are dead!", 0, GameField.size + 5);
+                            gameOver = true;
+                            break;
+                        }
+                        //Сбросить ловушку
+                        gf.field[player.pos.X, player.pos.Y] = 0;
+                        PutCharToConsole('P', player.pos.X , player.pos.Y);
+                    }
+                    //Игрок идет по пустой клетке
+                    else
+                    {
+                        PutCharToConsole('P', player.pos.X , player.pos.Y);
+                    }
+                    //Отобразить текущее здоровье игрока
+                    ClearLineFrom(0, GameField.size + 5);
+                    PutStringToConsole("Player HP:" + player.hp.ToString(), 0, GameField.size + 5);
+                }
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        bool MovePlayer(ConsoleKeyInfo keyInfo)
+        {
+            bool moved = false;
+            switch (keyInfo.Key)
+            {
+                
+                case ConsoleKey.LeftArrow:
+                    {
+                        if (player.pos.X > 0)
+                        {
+                            moved = true;
+                            player.MoveLeft();
+                        }
+                        break;
+                    }
+                case ConsoleKey.UpArrow:
+                    {
+                        if (player.pos.Y > 0)
+                        {
+                            moved = true;
+                            player.MoveUp();
+                        }
+                        break;
+                    }
+                case ConsoleKey.RightArrow:
+                    {
+                        if (player.pos.X < GameField.size - 1)
+                        {
+                            moved = true;
+                            player.MoveRight();
+                        }
+                        break;
+                    }
+                case ConsoleKey.DownArrow:
+                    {
+                        if (player.pos.Y < GameField.size - 1)
+                        {
+                            moved = true;
+                            player.MoveDown();
+                        }
+                        break;
+                    }
+                case ConsoleKey.Q:
+                    {
+                        return moved;
+                    }
+             
+
+            }
+            return moved;
+        }
+        void PutCharToConsole(char ch,int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(ch);
+        }
+        void PutStringToConsole(string message, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(message);
+        }
+        static void ClearLineFrom(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("                                        ");
+        }
+    }
+}
---
+++ GamePrincess/GameField.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrincess
+{
+   
+  
+    class GameField
+    {
+        //Содержимое ячеек с пользователем и с принцессой. Используется для перичного отображеняи поля.
+        const int playerCellId = 100;
+        const int finishCellId = 200;
+        const int trapsCount = 10;
+        const int maxTrapDamage = 10;
+    
+        public int[,] field;
+        public Position finalPos;
+        public const int size = 10;
+      
+        public GameField()
+        {
+            
+            field = new int[size, size];
+            //Клетка с принцессой
+            finalPos = new Position(9, 9);
+            field[0, 0] = playerCellId;
+            field[finalPos.X, finalPos.Y] = finishCellId;
+            int counter = 0;
+            Random rnd = new Random();
+            while (counter< trapsCount)
+            {
+                int x = rnd.Next(0, size);
+                int y = rnd.Next(0, size);
+                //не будем окружать вплотную стартовую позицию игрока и позицию принцессы ловушками.
+                if ((x < 3 && y < 3) || (x > 7 && y > 7))
+                    continue;
+                if (field[x, y] == 0)
+                {
+                    field[x, y] = rnd.Next(1,maxTrapDamage);
+                    counter++;
+                }
+            }
+            
+        }
+        public void Print(bool showTraps=false)
+        {
+            for (int i=0;i< size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                   Console.SetCursorPosition(i, j);
+                   switch(field[i, j])
+                    {
+                        case 0:
+                            {
+                                Console.Write('.');
+                                break;
+                            }
+                        case playerCellId:
+                            {
+                                Console.Write("P");
+                                break;
+                            }
+                        case finishCellId:
+                            {
+                                Console.Write("F");
+                                break;
+                            }
+                        default:
+                            {
+                                if(showTraps)
+                                    Console.Write(field[i, j]);
+                                else
+                                    Console.Write('.');
+                                break;
+                            }
+                    }
+                       
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
