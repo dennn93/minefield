66cb0118656054bddf2485e7c97b79b9d3eacf9b
dennn93 <dennn93@mail.ru>
2020-01-17 23:01:15 +0300

    1st commit

---
+++ GamePrincess/Position.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrincess
+{
+    class Position:ICloneable
+    {
+        public int X;
+        public int Y;
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public object Clone()
+        {
+            return new Position(X, Y);
+        }
+        public static bool operator ==(Position p1, Position p2)
+        {
+            return (p1.X == p2.X && p1.Y == p2.Y);
+        }
+        public static bool operator !=(Position p1, Position p2)
+        {
+            return (p1.X != p2.X || p1.Y != p2.Y);
+        }
+    }
+}
---
+++ GamePrincess/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePrincess
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Player player = new Player();
+                GameField gameField = new GameField();
+                Game game = new Game(gameField, player);
+                //CHEAT: установить в true, чтобы видеть ловушки
+                bool showTraps=false;
+                game.Start(showTraps);
+
+                Console.WriteLine("\nOne more time?(y/n)");
+                bool stopAsk = false;
+                while (!stopAsk)
+                {
+                    string answer = Console.ReadLine();
+                    switch (answer)
+                    {
+                        case "n":
+                            {
+                                stopAsk = true;
+                                exit = true;
+                                break;
+                            }
+                        case "y":
+                            {
+                                stopAsk = true;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Specify either 'y' or 'n'");
+                                break;
+                            }
+                    }
+                }    
+            }
+        }
+    }
+}
